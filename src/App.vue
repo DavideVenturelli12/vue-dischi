5997a1aeac4f10f93288b7fea041cfc2a975f627
Davide Venturelli <dventurelli90@gmail.com>
2022-04-08 19:25:30 +0200

    start

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Header />
+    <Main />
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+import Main from "./components/Main.vue";
+
+export default {
+  name: "App",
+  components: {
+    Header,
+    Main,
+  },
+};
+</script>
+
+<style lang="scss">
+* {
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+}
+</style>
